f1d9532b5733672895e57ae1139ae9f096822b4c
Justin Jones <5347951+justinejones@users.noreply.github.com>
2019-10-19 12:51:33 -0500

    added the hello world source file

---
+++ src/hello_world.py
@@ -0,0 +1,10 @@
+"""Hello World starter file."""
+
+
+def main_method():
+    """Called when the file is executed."""
+    print("Hello World!")
+
+
+if __name__ == '__main__':
+    main_method()
--- src/python_for_beginners.py
+++
@@ -1,7 +0,0 @@
-
-def main_method():
-    print("Hello World!")
-
-
-if __name__ == '__main__':
-    main_method()
