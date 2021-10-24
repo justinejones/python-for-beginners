98a885dc945447316076c0a648a88b2648ff8577
Justin Jones <5347951+justinejones@users.noreply.github.com>
2021-10-24 15:19:18 -0500

    added example of writing csv file.  added test for writing csv file (#17)

--- src/utilities/file_processing/writing_files.py
+++ src/utilities/file_processing/writing_files.py
@@ -1,9 +1,10 @@
 """The following module demonstrates how to read files"""
 import json
+import csv
 
 
 def write_json_file(file_name, json_file_content):
-    """Writes JSON file.
+    """Write JSON file.
 
     :type file_name:
     :param file_name:
@@ -14,3 +15,25 @@ def write_json_file(file_name, json_file_content):
     """
     with open(file_name, 'w') as json_file:
         json.dump(json_file_content, json_file)
+
+
+def write_csv_file(file_name, headers, csv_file_content):
+    """Example of how to write a csv file.
+
+    :type file_name:
+    :param file_name:
+
+    :type headers:
+    :param headers:
+
+    :type csv_file_content:
+    :param csv_file_content:
+
+    :raises:
+
+    :rtype:
+    """
+    with open(file_name, 'w') as csv_file:
+        csv_writer = csv.writer(csv_file)
+        csv_writer.writerow(headers)
+        csv_writer.writerows(csv_file_content)
