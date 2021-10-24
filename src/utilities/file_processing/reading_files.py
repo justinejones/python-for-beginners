a3e0148b3132edc998b36b6bf53b9af42e08e2da
Justin Jones <5347951+justinejones@users.noreply.github.com>
2021-10-24 12:07:09 -0500

    added file for reading json files

---
+++ src/utilities/file_processing/reading_files.py
@@ -0,0 +1,17 @@
+"""The following module demonstrates how to read files"""
+import json
+
+
+def read_json_file(file_to_read: str) -> dict:
+    """Read a json file.
+
+    :type file_to_read:str:
+    :param file_to_read:str:
+
+    :raises:
+
+    :rtype: dict
+    """
+    json_file = dict()
+
+    return json_file
