0ecc9ae79f79997949eb8b995076789b8e15e375
Justin Jones <5347951+justinejones@users.noreply.github.com>
2021-10-24 15:17:29 -0500

    added example of writing csv file.  added test for writing csv file

--- tests/utilities/file_processing/test_writing_files.py
+++ tests/utilities/file_processing/test_writing_files.py
@@ -7,7 +7,7 @@ path = Path(__file__).parent.absolute()
 
 
 def test_writing_json_files():
-    """Tests reading a json file.
+    """Tests writing a json file.
 
     :raises:
 
@@ -27,3 +27,28 @@ def test_writing_json_files():
     response = reading_files.read_json_file(full_path)
     assert response
     assert response == content
+
+
+def test_writing_csv_files():
+    """Tests writing a csv file.
+
+    :raises:
+
+    :rtype:
+    """
+    x = datetime.datetime.now()
+    headers = ["Header1", "Header2", "Header3"]
+    content = [["Column1", "Column2", "Column3"],
+               ["Row2C1", "Row2C2", "Row2C3"],
+               ["Row3C1", "Row3C2", "Row3C3"]
+               ]
+    file_name = f"/test_writing_csv_file_{x}.csv"
+    full_path = f"{path}{file_name}"
+    writing_files.write_csv_file(full_path, headers, content)
+    response = reading_files.reading_csv_file(full_path)
+    combine = list()
+    combine.append(headers)
+    for row in content:
+        combine.append(row)
+    assert response
+    assert response == combine
